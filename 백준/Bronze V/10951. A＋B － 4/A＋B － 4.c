a6f2dfc8b46d1b0396cb595b670cc2309c15c623
Yewon Han <129031924+yewoonn@users.noreply.github.com>
2024-02-14 21:38:14 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze V/10951. A＋B － 4/A＋B － 4.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main() {
+	int answer[100];
+	int iNum1, iNum2;
+	int iCnt,iCheck;
+
+
+	iCnt = 0;
+
+	while (scanf("%d%d", &iNum1, &iNum2)!= EOF) {
+
+
+		if (iNum1 < 0 || iNum1 > 10 || iNum2 < 0 || iNum2 > 10) {
+			printf("범위에 맞는 숫자를 입력하세요.\n");
+		}
+		else {
+			answer[iCnt] = iNum1 + iNum2;
+			printf("%d\n", answer[iCnt]);
+			iCnt = iCnt + 1;
+		}
+
+	}
+
+	return 0;
+}
