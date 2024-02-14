a0987fba9f924dabb59f25eda49c17a82589eec5
Yewon Han <129031924+yewoonn@users.noreply.github.com>
2024-02-14 21:39:17 +0900

    [Bronze III] Title: 최소, 최대, Time: 180 ms, Memory: 4900 KB -BaekjoonHub

---
+++ 백준/Bronze III/10818. 최소， 최대/최소， 최대.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int iNum, iMin, iMax;
+	int iArr[1000000];
+
+	scanf("%d", &iNum);
+
+	if (iNum <= 1000000) {
+		for (int i = 0; i < iNum; i++) {
+			scanf("%d", &iArr[i]);
+		}
+
+		iMin = iArr[0];
+		for (int i = 0; i < iNum; i++) {
+			if (iArr[i] > iMin) {
+				continue;
+			}
+			else {
+				iMin = iArr[i];
+			}
+		}
+
+		iMax = iArr[0];
+		for (int i = 0; i < iNum; i++) {
+			if (iArr[i] < iMax) {
+				continue;
+			}
+			else {
+				iMax = iArr[i];
+			}
+		}
+		printf("%d ", iMin);
+		printf("%d", iMax);
+
+	}
+	
+	return 0;
+}
