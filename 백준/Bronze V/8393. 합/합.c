cca183189bcfeafd76eb8248756a4a52b7854d78
Yewon Han <129031924+yewoonn@users.noreply.github.com>
2024-02-14 21:34:13 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze V/8393. 합/합.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int iInput;
+	int iRes;
+	int iCheck;
+
+	//printf("n (1 <= n <= 10,000)\n");
+	scanf("%d", &iInput);
+
+	iRes = 0;
+
+	if (iInput >= 1 && iInput <= 10000) {
+		for (int i = 0; i < iInput; i++) {
+			iCheck = i + 1;
+			iRes = iRes + iCheck;
+		}
+
+		printf("%d", iRes);
+	}
+	else {
+		return 0;
+	}
+
+
+	return 0;
+}
