b581368eb5c1d7d2ef66a8e815c7f0e234d329c2
Yewon Han <129031924+yewoonn@users.noreply.github.com>
2024-02-14 21:35:59 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze V/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int iAgain;
+	int iNum1, iNum2;
+	int iRes, iTurn, iFlag;
+	int i, ar[100];
+
+	scanf("%d\n", &iAgain);
+
+	iFlag = 0;
+	iTurn = 0;
+	i = 0;
+
+	while (iTurn != iAgain) {
+
+		scanf("%d %d", &iNum1, &iNum2);
+
+		
+		if (iNum1 > 0 && iNum1 < 10 && iNum2 > 0 && iNum2 < 10) {
+			ar[i] = iNum1 + iNum2;
+			i++;
+			iTurn = iTurn + 1;
+		}
+		else {
+			continue;
+		}
+
+		
+	}
+
+	for (int j = 0; j < iAgain; j++) {
+		iFlag = j + 1;
+		printf("Case #%d: %d\n", iFlag, ar[j]);
+	}
+
+	return 0;
+}
